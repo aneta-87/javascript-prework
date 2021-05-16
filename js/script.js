27399882539b789f0c4088f29c032702cbdba836
Aneta Giza <anetagiza@interia.eu>
2021-05-16 16:51:07 +0200

    modified: js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
