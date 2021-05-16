54f888bcc8b055b21f3aa45fa69049cef3778ded
Aneta Giza <anetagiza@interia.eu>
2021-05-16 17:00:19 +0200

    mogified: js/script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
 
-if(playerInput == '2'){
-  playerMove = 'papier';
+if(randomNumber == 2){
+  computerMove = 'papier';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
 
-if(playerInput == '3'){
-  playerMove = 'nożyce';
+if(randomNumber == 3){
+  computerMove = 'nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
